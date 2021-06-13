c58bc0eefa1084a9ac96cfabac6ae82d86b88629
Alan Cortes <ing.ti.upmh@gmail.com>
2021-06-13 01:03:30 -0500

    1.- Se crea la carepta 'pages' se mueven las páginas tab1, tab2,etc., Se modifica la ruta del tabs-routing.module.ts y se crea y agrega la apikey de News API en el enviroment.prod.ts y enviroment.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apikey: '50be7885985b4ba38ccebf9fd4d83731'
+  
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apikey: '50be7885985b4ba38ccebf9fd4d83731'
 };
 
 /*

712742a8ac367b91a6c9897a2e66d44c7613718d
Alan Cortes <ing.ti.upmh@gmail.com>
2021-06-13 01:47:38 -0500

    Se inyecta el HttpClient y se consume el servicio haciendo uso de ngOnInit(hace las peticiones al servicio)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/services/noticias.service.ts
+++ src/app/services/noticias.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,12 @@ import { Injectable } from '@angular/core';
 })
 export class NoticiasService {
 
-  constructor() { }
+  //Se inyecta el HttpClient
+  constructor( private http: HttpClient) { }
+
+  //Obtener Top Head Lines 
+  getTopHeadLines() {
+    return this.http.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=50be7885985b4ba38ccebf9fd4d83731`);
+  }
+
 }

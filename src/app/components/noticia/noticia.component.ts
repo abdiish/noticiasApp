bed131b075542caf93f2e7b84ca7a3ce6184c5a4
Alan Cortes <ing.ti.upmh@gmail.com>
2021-06-19 21:10:53 -0500

    Se implementa el uso de ActionSheetController

--- src/app/components/noticia/noticia.component.ts
+++ src/app/components/noticia/noticia.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Article } from 'src/app/interfaces/interfaces';
 
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
-
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-noticia',
@@ -15,13 +15,48 @@ export class NoticiaComponent implements OnInit {
   @Input() noticia: Article;
   @Input() indice: number;
 
-  constructor(private iab: InAppBrowser) { }
+  constructor(private iab: InAppBrowser, private actionSheetContrl: ActionSheetController) { }
 
   ngOnInit() {}
 
   abrirNoticia() {
     console.log('Noticia', this.noticia.url);
     const browser = this.iab.create(this.noticia.url, '_system');
+  }
+
+  async lanzarMenu(){
+
+    const actionSheet = await this.actionSheetContrl.create({
+      cssClass: 'my-custom-class',
+      buttons: [{
+        text: 'Compartir',
+        icon: 'share',
+        cssClass: 'action-dark',
+        handler: () => {
+          console.log('Share clicked');
+        }
+      }, {
+        text: 'Favorito',
+        icon: 'heart',
+        cssClass: 'action-dark',
+        handler: () => {
+          console.log('Favorite clicked');
+        }
+      }, {
+        text: 'Cancelar',
+        icon: 'close',
+        cssClass: 'action-dark',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+
+    const { role } = await actionSheet.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+
   }
 
 }
